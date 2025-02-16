57f036aae0eaf39f4833a9598c891c99058bdddf
Yaroslav <varathius@gmail.com>
2025-02-16 11:53:11 +0200

    hw7: fixed types

--- homework-07/src/arrow-functions.ts
+++ homework-07/src/arrow-functions.ts
@@ -5,11 +5,11 @@
  * Результат роботи стрілкової функції вивести в консоль
  */
 // Стрілкова функція для додавання елементів масиву однією строкою
-const sumArrayElementsOneLine = (arr: (string | number)[]): string | number =>
+const sumArrayElementsOneLine = (arr: (string[] | number[])): string | number =>
     typeof arr[0] === 'string' ? arr.join('') : arr.reduce((sum, current) => (sum as number) + (current as number));
 
 // Стрілкова функція для додавання елементів масиву через умову
-const sumArrayElementsIfElse = (data: (string | number)[]): string | number => {
+const sumArrayElementsIfElse = (data: (string[] | number[])): string | number => {
     if (typeof data[0] === 'string') {
         return data.join('');
     } else {
@@ -20,9 +20,11 @@ const sumArrayElementsIfElse = (data: (string | number)[]): string | number => {
 // Створення масивів рядків і чисел
 const arrowStringArray = ['Hello', ' ', 'World', '!'];
 const arrowNumberArray = [1, 2, 3, 4];
+//const testArray = ['Hello', 1];
 
 // Виведення результатів роботи функції в консоль
 console.log(`Конкатенація рядків: ${sumArrayElementsOneLine(arrowStringArray)}`);
+//console.log(`Конкатенація змішаних типів: ${sumArrayElementsOneLine(testArray)}`); - не компілюється через неприйнятний для функції тип.
 console.log(`Сума чисел: ${sumArrayElementsOneLine(arrowNumberArray)}`);
 console.log(`Конкатенація рядків (if-else): ${sumArrayElementsIfElse(arrowStringArray)}`);
 console.log(`Сума чисел (if-else): ${sumArrayElementsIfElse(arrowNumberArray)}`);
--- homework-07/src/functions.ts
+++ homework-07/src/functions.ts
@@ -5,25 +5,57 @@
  * Результат роботи функції вивести в консоль
  */
 
-// Спробував просто типізувати дані, які приймає функція - всерівно ругається через використання додавання з різними типами даних
+// Залишив ти (number | string)[] але додав перевірку на тип масива, який отримав
 function lazySumArrayElements(data: (string | number)[]): string | number {
-    return data.reduce((sum, current) => sum + current);
+    console.log('Вхідні дані:', data);
+
+    if (data.length === 0) {
+        throw new Error('Масив порожній');
+    }
+
+    const isStringArray = data.every(element => typeof element === 'string');
+    const isNumberArray = data.every(element => typeof element === 'number');
+
+    if (!isStringArray && !isNumberArray) {
+        throw new Error('Невідповідні типи в масиві');
+    }
+
+    if (isStringArray) {
+        return data.join('');
+    } else {
+        return data.reduce((sum, current) => (sum as number) + (current as number));
+    }
 }
 
-// Тепер спробую в функції через умову розділити роботу в залежності від типу даних
-function extSumArrayElements(data: (string | number)[]): string | number {
+// Тут функція одразу не приймає змішаний масив;
+function sumArrayElements(data: string[] | number[]): string | number {
+    console.log(`Вхідні дані: ${data}`);
+
+    if (data.length === 0) {
+        return 'Масив порожній';
+    }
     if (typeof data[0] === 'string') {
         return data.join('');
     } else {
-        return data.reduce((sum, current) => (sum as number) + (current as number), 0);
+        return data.reduce((sum, current) => (sum as number) + (current as number));
     }
 }
 
 // Створення масиви рядків і чисел
 const stringArray = ['Hello', ' ', 'World', '!'];
 const numberArray = [1, 2, 3, 4];
-console.log(`Конкатенація рядків: ${lazySumArrayElements(stringArray)}`);
-console.log(`Сума чисел: ${lazySumArrayElements(numberArray)}`);
+const mixedArray = ['Hello', 3];
+const emptyArray: number[] = [];
 
-console.log(`Конкатенація рядків: ${extSumArrayElements(stringArray)}`);
-console.log(`Сума чисел: ${extSumArrayElements(numberArray)}`);
+try {
+    console.log(`Цифровий масив: ${sumArrayElements(numberArray)}`);
+    console.log(`Цифровий масив: ${lazySumArrayElements(numberArray)}`);
+    console.log(`Масив рядків: ${sumArrayElements(stringArray)}`);
+    console.log(`Масив рядків: ${lazySumArrayElements(stringArray)}`);
+    console.log(`Пустий масив: ${sumArrayElements(emptyArray)}`);
+    //console.log(`Пустий масив: ${lazySumArrayElements(emptyArray)}`); - закоментив, щоб код не зупинявся.
+    console.log(`Масив із різними типами: ${lazySumArrayElements(mixedArray)}`);
+    // console.log(sumArrayElements(mixedArray) - закоментовано, бо код не компілюється при спробі передати невідповідний тип у функцію.
+} catch (error) {
+    console.error('Помилка:', (error as Error).message);
+}
